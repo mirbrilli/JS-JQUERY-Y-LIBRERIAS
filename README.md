0433cb59fc87c83215f26128e576db7a4a578fe7
Miriam Brillaxis <miriam.brillaxis@gmail.com>
2024-10-11 17:50:17 +0200

    js jquery y librerias

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # JS-JQUERY-Y-LIBRERIAS
- 
+ Para cargar nuestros archivos de JS lo haremos justo al final del <body></body>
+
+ Las librerias de JS es un conjunto de funciones ya definidas que podemos usar en nuestro código. Las librerías de JS las enlazaremos en nuestro <head></head> o algo final del <body></body>, pero siempre antes de nuestro propio archivos de JS. 
+
+ Podemos cargar librerias de JS de forma local (descargando el código y enlazandolo en mi web o de froma remota)
+
+ AOS -->Es una libreria de JS para asociar efectos de movimiento a elementos de mi web. 
--- js/script.js
+++ js/script.js
@@ -2,6 +2,20 @@ const html = document.querySelector("html");
 html.setAttribute("data-bs-theme", "dark");
 
 document.addEventListener("DOMContentLoaded", () => {
+    // EVENTO Y EFECTO CON JQUERY 
+    $("#myBtn").click(function(){
+      alert("Has hecho click!")
+    });
+
+    // <!-- TYPED.JS  -->  
+      var typed = new Typed('#element', {
+      strings: ['<i>Welcome to my web!</i>', 'Bienvenue!'],
+      typeSpeed: 50,
+      loop: true,
+
+      });
+         
+
     // --- Create LightBox
     const galleryGrid = document.querySelector(".gallery-grid");
     const links = galleryGrid.querySelectorAll("a");
